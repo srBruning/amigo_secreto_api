fea7b6c5160f26b1df5e06895ecf77750afc25dd
Diego Bruning <diego.brg92@gmail.com>
2020-12-16 13:31:13 -0300

    Habilitar o suporte ao CORS

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ const routes = require('./routes');
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization, x-access-token');
+    next();
+  });
+
 app.use(routes);
 
 app.listen(3636);
